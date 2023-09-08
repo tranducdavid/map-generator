3c48860adbdde6a3883d099be0aae50905b839bc
David Tran Duc <davidtranduc8@gmail.com>
2023-09-08 23:55:12 +0200

    Add tsdoc to `createGameMap`

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,17 @@
 import _ from 'lodash'
 import { GameMap, TileType } from './types'
 
+/**
+ * Creates a game map with specified dimensions.
+ *
+ * @param width - The width of the game map.
+ * @param height - The height of the game map.
+ * @param defaultTileType - The default tile type to initialize the map with. Defaults to `null`.
+ * @returns A new `GameMap` object with the specified dimensions and initialized tiles and edges.
+ *
+ * @example
+ * const map = createGameMap(10, 10, TileType.WALL);
+ */
 export const createGameMap = (
   width: number,
   height: number,
