4442aebb7283e3c2c6906c95cd1f5a354f7c905a
David Tran Duc <davidtranduc8@gmail.com>
2023-09-09 08:25:02 +0200

    Fix profiling types

--- src/utils/profiling.ts
+++ src/utils/profiling.ts
@@ -1,9 +1,9 @@
 let profileDepth = 0
 
-export const profile = (fn: (...args: any[]) => any, customLabel?: string) => {
+export function profile<T extends (...args: any[]) => any>(fn: T, customLabel?: string): T {
   const label = customLabel || fn.name || 'Anonymous Function'
 
-  return (...args: any[]) => {
+  return ((...args: Parameters<T>): ReturnType<T> => {
     const spaces = ' '.repeat(profileDepth * 4)
     profileDepth++
 
@@ -16,5 +16,5 @@ export const profile = (fn: (...args: any[]) => any, customLabel?: string) => {
     profileDepth--
 
     return result
-  }
+  }) as T
 }
